cff70eb9527e3a28adf8adf2239c68682273afeb
Kirill Emelyanov <k.emelyanov070@gmail.com>
2024-08-26 20:57:51 +0300

    init repo

---
+++ src/main/java/emv/backend/spring/fiesta/fiestaproject/FiestaProjectApplication.java
@@ -0,0 +1,15 @@
+package emv.backend.spring.fiesta.fiestaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiestaProjectApplication
+{
+
+  public static void main(String[] args)
+  {
+    SpringApplication.run(FiestaProjectApplication.class, args);
+  }
+
+}
---
+++ src/test/java/emv/backend/spring/fiesta/fiestaproject/FiestaProjectApplicationTests.java
@@ -0,0 +1,15 @@
+package emv.backend.spring.fiesta.fiestaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiestaProjectApplicationTests
+{
+
+  @Test
+  void contextLoads()
+  {
+  }
+
+}
